30e2095e57798feb49eba473f17c4fa6be801da9
Jihee Ryu <chrisjihee@gmail.com>
2016-08-24 15:55:06 +0900

    첫째날 실습 완료

--- address4C/main.cpp
+++ address4C/main.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Person.h"
 #include "Telephone.h"
-void main() {
-	Telephone *data[MAX_TELEPHONE] = { NULL };
-	int n = 0;
 
+Telephone *data[MAX_TELEPHONE] = { NULL };
+int n = 0;
+
+void load() {
 	FILE *ifp = fopen(DATAFILE, "r");
 	char temp[80];
 	fscanf(ifp, "%s%s%s%s%s%s",
 		&temp, &temp, &temp, &temp, &temp, &temp);
-	for(int i=0; 1; i++) {
+	for (int i = 0; 1; i++) {
 		int id;
 		char *name = (char*)malloc(sizeof(char) * 20);
 		char *phone = (char*)malloc(sizeof(char) * 20);
@@ -23,9 +25,48 @@ void main() {
 		data[i] = new Telephone(id, name, phone, email, city, year);
 		n++;
 	}
+	fclose(ifp);
+}
 
+void list() {
+	printf("-------------------------------------------------------------------\n");
 	for (int i = 0; i < n; i++)
 		printf("%s\n", data[i]->ToString());
+	printf("-------------------------------------------------------------------\n");
+}
 
+int list2(char *keyword) {
+	int r = 0;
+	printf("-------------------------------------------------------------------\n");
+	for (int i = 0; i < n; i++) {
+		char *str = data[i]->ToString();
+		if (strstr(str, keyword)) {
+			printf("%s\n", str);
+			r++;
+		}
+	}
+	printf("-------------------------------------------------------------------\n");
+	return r;
+}
 
+void search(char *keyword) {
+	int r = list2(keyword);
+	printf("==> 검색결과 수 : %d건\n", r);
+}
+
+void main(int argc, char **argv) {
+	if (argc < 2) {
+		printf("사용법: address4C 명령\n");
+		printf("  * 명령 :\n");
+		printf("    - LIST : 주소록 사람들 보여주기\n");
+		printf("    - SEARCH 검색어 : 주소록 사람들 검색하기\n");
+		return;
+	}
+	load();
+	if (strcmp(argv[1], "LIST") == 0) {
+		list();
+	}
+	else if (strcmp(argv[1], "SEARCH") == 0) {
+		search(argv[2]);
+	}
 }
